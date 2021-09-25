2107699fff8c46edb0358542d1c7ff00248c810f
Alexander Trakhimenok <alex@trakhimenok.com>
2021-09-25 09:33:24 +0100

    1st draft

---
+++ end2end.go
@@ -0,0 +1,38 @@
+package end2end
+
+import (
+	"context"
+	"github.com/strongo/dalgo"
+	"sync"
+	"testing"
+)
+
+func EndToEnd(t *testing.T, db dalgo.Database) {
+	if t == nil {
+		panic("t == nil")
+	}
+	if db == nil {
+		panic("db == nil")
+	}
+
+	ctx := context.Background()
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		t.Run("single", func(t *testing.T) {
+			testSingleOperations(ctx, t, db)
+		})
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		t.Run("multi", func(t *testing.T) {
+			testMultiOperations(ctx, t, db)
+		})
+		wg.Done()
+	}()
+	wg.Wait()
+}
